595501221df2ff07d3b42dbcf1e57ab1c43c36b6
Yann ENR Courtage <yann@enrcourtage.fr>
2025-12-29 15:48:03 +0100

    temp: désactiver Configurateur pour stabiliser déploiement - reconstruction CAD en cours

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ import AppLayout from './components/AppLayout.jsx';
 import Crm from './pages/Crm.jsx';
 import Admin from './pages/Admin.jsx';
 import ProfitabilitySimulator from './pages/ProfitabilitySimulator.jsx';
-import Configurateur from './pages/Configurateur.jsx';
+// TEMPORAIRE: Configurateur désactivé pour debug déploiement Vercel
+// Reconstruction en cours avec approche CAD-style
+// import Configurateur from './pages/Configurateur.jsx';
 import CDP from './pages/CDP.jsx';
 import { Toaster } from './components/ui/toaster.jsx';
 import { ProjectProvider } from './contexts/ProjectContext.jsx';
@@ -77,6 +79,7 @@ function AppContent() {
               </ProtectedRoute>
             }
           />
+          {/* TEMPORAIRE: Route désactivée pendant reconstruction CAD-style
           <Route
             path="configurateur"
             element={
@@ -85,6 +88,7 @@ function AppContent() {
               </ProtectedRoute>
             }
           />
+          */}
           <Route
             path="cdp"
             element={
