ef0d6e8ed361fd75e70ccaf889988e30a860491a
Yann ENR Courtage <yann@enrcourtage.fr>
2026-01-02 17:46:26 +0100

    fix(conf): hide main eave marker if auvent, adjust sabliere height

--- src/components/configurator/structure/Auvent.jsx
+++ src/components/configurator/structure/Auvent.jsx
@@ -212,9 +212,13 @@ export function Auvent({ length, eaveHeight, ridgeHeight, roofPitch, buildingWid
             {/* STRUCTURE */}
             {frames}
             {/* Longitudinal Beam (Sablière) at Tip - Massive */}
-            {/* Longitudinal Beam (Sablière) at Tip - Reduced 20%, Lowered 20cm */}
+            {/* Longitudinal Beam (Sablière) at Tip - Reduced 20%, Adjusted Height */}
             <mesh
-                position={[auventWidth, -auventWidth * Math.tan(angleRad) - 0.1 + 0.3, length / 2]}
+                position={[
+                    auventWidth,
+                    -auventWidth * Math.tan(angleRad) - 0.1 + (buildingType === 'monopente' ? 0.4 : 0.25),
+                    length / 2
+                ]}
                 rotation={[0, 0, 0]}
                 material={structureMaterial}
             >
