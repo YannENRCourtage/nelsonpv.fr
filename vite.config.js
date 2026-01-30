53e5d0dd293d6f9fd6cc0b99c3eaa94107d7c225
Yann ENR Courtage <yann@enrcourtage.fr>
2026-01-30 16:54:07 +0100

    chore: remove Cesium 3D tab and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { createLogger, defineConfig } from 'vite';
-import cesium from 'vite-plugin-cesium';
 import inlineEditPlugin from './plugins/visual-editor/vite-plugin-react-inline-editor.js';
 import editModeDevPlugin from './plugins/visual-editor/vite-plugin-edit-mode.js';
 import iframeRouteRestorationPlugin from './plugins/vite-plugin-iframe-route-restoration.js';
@@ -239,7 +238,6 @@ export default defineConfig({
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin(), iframeRouteRestorationPlugin(), selectionModePlugin()] : []),
 		react(),
-		cesium(),
 		addTransformIndexHtml
 	],
 	server: {
