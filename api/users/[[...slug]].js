5193885b9b2c4af7cf57e5f4e0fb1b3aebffe7d5
Yann ENR Courtage <yann@enrcourtage.fr>
2026-07-08 10:21:32 +0200

    fix(vercel): ignore authMiddleware.js as serverless function to respect hobby plan limits

--- api/users/[[...slug]].js
+++ api/users/[[...slug]].js
@@ -1,6 +1,6 @@
 import { prisma } from '../../src/lib/prisma.js'
 import bcrypt from 'bcryptjs'
-import { withAdmin } from '../common/authMiddleware.js'
+import { withAdmin } from '../common/_authMiddleware.js'
 
 async function handler(req, res) {
     // CORS headers
